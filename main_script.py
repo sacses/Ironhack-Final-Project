cf56b8f260cfdb18a4cad813693c6d850091a568
Fran Tamarit <fcodpaula.tamarit@gmail.com>
2020-04-15 19:35:36 +0200

    added argparse

--- main_script.py
+++ main_script.py
@@ -3,19 +3,38 @@ from package1.wrangle import wrangle
 from package2.variables import *
 from package1.analysis import analysis
 from package1.load import complete_load
-#import argparse
+from dotenv import load_dotenv
+import argparse
+import os
 
-"""
+
 def parse():
-    parser = argparse.ArgumentParser(description='Create predictions over a given time horizon')
-    parser.add_argument('threshold', type=float, help='Maximum MAPE threshold')
+    parser = argparse.ArgumentParser(description='Input the desired variables for your forecast')
 
-"""
+    parser.add_argument('-t', '--threshold', type=float, metavar='',
+                        default=os.environ.get('MAPE_THRESHOLD'),
+                        help='Maximum MAPE threshold')
+    parser.add_argument('-u', '--units', type=str, metavar='',
+                        default=os.environ.get('INPUT_FREQ'),
+                        help='Time units such as M, W or D')
+    parser.add_argument('-c', '--x_cutoff', type=int, metavar='',
+                        default=os.environ.get('X_CUTOFF'),
+                        help='How often is the forecast x-validated')
+    parser.add_argument('-f', '--x_forecast', type=int, metavar='',
+                        default=os.environ.get('X_FCST'),
+                        help='Horizon of the forecast in x-validation')
+    parser.add_argument('-H', '--horizon', type=int, metavar='',
+                        default=os.environ.get('HORIZON'),
+                        help='Horizon of the forecast')
+    return parser.parse_args()
+
 
 def main():
+    load_dotenv()
+    args = parse()
     MM = acquire()
     dict_series_end_start = wrangle(MM)
-    output_df = analysis(MM, dict_series_end_start, grid)
+    output_df = analysis(MM, dict_series_end_start, grid, args)
     complete_load(output_df)
 
 
--- package1/analysis.py
+++ package1/analysis.py
@@ -1,7 +1,5 @@
-import os
 import numpy as np
 import pandas as pd
-from dotenv import load_dotenv
 from datetime import datetime
 from matplotlib import pyplot as plt
 from fbprophet import Prophet
@@ -9,9 +7,9 @@ from fbprophet.diagnostics import cross_validation
 from fbprophet.plot import add_changepoints_to_plot
 
 
-def get_df(market, MM, dict_series_end_start):
+def get_df(market, mm, timeframe):
     try:
-        series = MM.loc[dict_series_end_start[market][0]:dict_series_end_start[market][1],
+        series = mm.loc[timeframe[market][0]:timeframe[market][1],
                  market].iloc[:-1].fillna(0)
         df_output = series.reset_index().rename({'index': 'ds', market: 'y'},
                                                 axis=1)
@@ -21,13 +19,13 @@ def get_df(market, MM, dict_series_end_start):
         return print(f"There isn't enough historic data to predict {market} volumes or {market} is not a valid market.")
 
 
-def get_test_val(market, grid, MM, dict_series_end_start):
+def get_test_val(market, grid, mm, timeframe):
     return round(len(get_df(market,
-                            MM, dict_series_end_start)) * grid[market]['initial'])
+                            mm, timeframe)) * grid[market]['initial'])
 
 
-def fit_model_kwargs(market, grid, MM, dict_series_end_start):
-    X = get_df(market, MM, dict_series_end_start)
+def fit_model_kwargs(market, grid, mm, timeframe):
+    X = get_df(market, mm, timeframe)
     if market in ['GBP', 'EUR', 'CAD', 'USD', 'AUD']:
         X.loc[X['ds'] > '2020-03-02', 'y'] = None
     print(f"-----------------Training {market} model------------------")
@@ -35,14 +33,14 @@ def fit_model_kwargs(market, grid, MM, dict_series_end_start):
     return model.fit(X)
 
 
-def create_forecast_df(market, period, frequency, grid, MM, dict_series_end_start):
-    model = fit_model_kwargs(market, grid, MM, dict_series_end_start)
+def create_forecast_df(market, period, frequency, grid, mm, timeframe):
+    model = fit_model_kwargs(market, grid, mm, timeframe)
 
     future = model.make_future_dataframe(periods=period, freq=frequency)
     forecast = model.predict(future)
     forecast = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
     forecast[['yhat', 'yhat_lower']] = forecast[['yhat', 'yhat_lower']].clip(0, )
-    forecast_df = pd.merge(forecast, get_df(market, MM, dict_series_end_start),
+    forecast_df = pd.merge(forecast, get_df(market, mm, timeframe),
                            how='left', on='ds')
 
     return forecast_df
@@ -65,14 +63,14 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return round(np.mean(np.abs((y_true - y_pred) / y_true)) * 100, 1)
 
 
-def model_error_kwargs(market, cutoff, fcst, units, grid, MM, dict_series_end_start):
-    model = fit_model_kwargs(market, grid, MM, dict_series_end_start)
+def model_error_kwargs(market, cutoff, fcst, units, grid, mm, timeframe):
+    model = fit_model_kwargs(market, grid, mm, timeframe)
     print(f"-----------------Cross validating {market} model------------------")
     cv_results = cross_validation(model=model,
                                   initial=pd.to_timedelta(get_test_val(market,
                                                                        grid,
-                                                                       MM,
-                                                                       dict_series_end_start),
+                                                                       mm,
+                                                                       timeframe),
                                                           unit=units),
                                   period=pd.to_timedelta(cutoff, unit=units),
                                   horizon=pd.to_timedelta(fcst, unit=units))
@@ -83,29 +81,29 @@ def model_error_kwargs(market, cutoff, fcst, units, grid, MM, dict_series_end_st
     return mape
 
 
-def model_score(grid, MM, dict_series_end_start):
-    freq = os.getenv('INPUT_FREQ')
-    x_cutoff = int(os.getenv('X_CUTOFF'))
-    x_fcst = int(os.getenv('X_FCST'))
-    scores = {i: model_error_kwargs(i, x_cutoff, x_fcst, freq, grid, MM, dict_series_end_start)
+def model_score(grid, mm, timeframe, args):
+    freq = args.units
+    x_cutoff = args.x_cutoff
+    x_fcst = args.x_forecast
+    scores = {i: model_error_kwargs(i, x_cutoff, x_fcst, freq, grid, mm, timeframe)
               for i in grid.keys()}
     return scores
 
 
-def get_threshold():
-    value = os.getenv('MAPE_THRESHOLD')
+def get_threshold(x):
+    value = x
     print(f"-----------------Threshold {value} set------------------")
     return float(value)
 
 
-def export_df(test_passed_scores, grid, MM, dict_series_end_start):
+def export_df(test_passed_scores, grid, mm, timeframe, args):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     output_df = pd.DataFrame()
-    horizon = int(os.getenv('HORIZON'))
-    freq = os.getenv('INPUT_FREQ')
+    horizon = args.horizon
+    freq = args.units
     for i in test_passed_scores:
-        market_df = create_forecast_df(i, horizon, freq, grid, MM, dict_series_end_start).assign(market=i,
-                                                                                           mape=test_passed_scores[i])
+        market_df = create_forecast_df(i, horizon, freq, grid, mm, timeframe).assign(market=i,
+                                                                                      mape=test_passed_scores[i])
         output_df = output_df.append(market_df)
 
     output_df.to_csv(f'data/processed/output{timestamp}.csv', index=False)
@@ -113,11 +111,7 @@ def export_df(test_passed_scores, grid, MM, dict_series_end_start):
     return output_df
 
 
-def analysis(mm, timeframe, grid):
-    load_dotenv()
-    MM = mm
-    dict_series_end_start = timeframe
-    dict_scores = model_score(grid, MM, dict_series_end_start)
-    threshold = get_threshold()
-    markets_passed_threshold = {i: dict_scores[i] for i in dict_scores if dict_scores[i] < threshold}
-    return export_df(markets_passed_threshold, grid, MM, dict_series_end_start)
+def analysis(mm, timeframe, grid, args):
+    dict_scores = model_score(grid, mm, timeframe, args)
+    markets_passed_threshold = {i: dict_scores[i] for i in dict_scores if dict_scores[i] < args.threshold}
+    return export_df(markets_passed_threshold, grid, mm, timeframe, args)
